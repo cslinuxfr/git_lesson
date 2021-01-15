b8b3ebc69d506ef8b3b13d44d2f0765a684d3d87
Cedric.C <cslinux.cch@gmail.com>
2021-01-15 08:51:40 +0100

    x -<=> y and square 42

--- src/my_square.py
+++ src/my_square.py
@@ -1,10 +1,8 @@
-def my_square(x):
+def my_square(y):
 	"""takes a value and returns the square value.
-<<<<<<< HEAD
-	"""	
-=======
-	use the ** operator
->>>>>>> 9b20889023adb9e3cb5d39356f479e8d980b505f
-	return(x ** 2)
 
-print(my_square(4))
+	use the ** operator
+	"""	
+	return(y ** 2)
+
+print(my_square(42))
